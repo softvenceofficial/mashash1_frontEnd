45f31816a27e0e7f950195e449ffcadde5a7928e
Niamulhuda-Nahid <niamulhuda6046@gmail.com>
2025-12-07 18:14:25 +0600

    feat: update layout and navigation in Dashboard components for improved user experience

--- src/components/Dashboard/MyFiles/RecentWork.tsx
+++ src/components/Dashboard/MyFiles/RecentWork.tsx
@@ -5,7 +5,7 @@ export default function RecentWork() {
   return (
     <section>
         <h4 className="text-lg font-medium mb-7">Recent Work</h4>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-15">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10 mr-5">
             {
             workingData.map((work) => (
                 <WorkingCard key={work.id} work={work} />
--- src/components/Dashboard/Trash/DeleteModal.tsx
+++ src/components/Dashboard/Trash/DeleteModal.tsx
@@ -12,16 +12,16 @@ export default function DeleteModal() {
     }
   return (
     <div className="">
-      <h2 className="text-2xl font-semibold">Permanently delete file</h2>
+      <h2 className="text-xl md:text-2xl font-semibold">Permanently delete file</h2>
       <Separator className="my-6 bg-gray-200 dark:bg-[#3B3B3B]" />
-      <p className="text-2xl">
+      <p className="md:text-xl">
         You sure you want to delete this file permanently? You won’t be able to
         restore it.
       </p>
       <Separator className="my-6 bg-gray-200 dark:bg-[#3B3B3B]" />
-        <div className="gap-6 flex items-center justify-end">
-            <Button onClick={() => close(["modal"])} className="text-2xl font-medium px-7 h-12 bg-white! text-black cursor-pointer">Cancel</Button>
-            <Button onClick={handleDelete} variant="destructive" className="text-2xl font-medium px-7 h-12 bg-[#EF4444]! cursor-pointer">Permanently delete</Button>
+        <div className="gap-6 flex max-sm:flex-col items-center justify-end">
+            <Button onClick={() => close(["modal"])} className="md:text-xl text-base font-medium px-7 h-12 bg-white! text-black cursor-pointer max-sm:w-full">Cancel</Button>
+            <Button onClick={handleDelete} variant="destructive" className="md:text-xl text-base font-medium px-7 h-12 bg-[#EF4444]! cursor-pointer max-sm:w-full">Permanently delete</Button>
         </div>
     </div>
   );
